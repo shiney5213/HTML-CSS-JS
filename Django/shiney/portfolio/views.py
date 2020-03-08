d10f9cc1480510fa9c6ebdef9c7749faac485698
shiney5213 <54365104+shiney5213@users.noreply.github.com>
2020-03-08 22:05:06 +0900

    Update views.py

--- Django/shiney/portfolio/views.py
+++ Django/shiney/portfolio/views.py
@@ -8,36 +8,65 @@ from django.views import View
 # Create your views here.
 
 def index(request):
-    file = request.FILES.get('filename', '')
-    filename = file._name
+	return HttpResponse('portfolio 보자!')
 
-    if file == '':
-        return HttpResponse('file을 다시 upload해주세요')
-    else:
-        fp = open(settings.BASE_DIR + '/static/dogonicon/' + filename, 'wb')
-        for chunk in file.chunks():
-            fp.write(chunk)
-        fp.close()
 
-    return HttpResponse('file upload success')
 
-class UploadView(View):
+
+class portfolioView(View):
     def get(self, request):
-        return render(request, 'dogonicon/image_upload.html')
+        return render(request, 'portfolio/port_main.html')
 
-    def post(self, request):
-        file = request.FILES.get('filename', '')
-        filename = file._name
+    # def post(self, request):
+    #     file = request.FILES.get('filename', '')
+    #     filename = file._name
 
-        if file == '':
-            return HttpResponse('file을 다시 upload해주세요')
-        else:
-            fp = open(settings.BASE_DIR + '/static/dogonicon/' + filename, 'wb')
-            for chunk in file.chunks():
-                fp.write(chunk)
-            fp.close()
+    #     if file == '':
+    #         return HttpResponse('file을 다시 upload해주세요')
+    #     else:
+    #         fp = open(settings.BASE_DIR + '/static/portfolio/' + filename, 'wb')
+    #         for chunk in file.chunks():
+    #             fp.write(chunk)
+    #         fp.close()
          
-        context= {'data': filename}
+    #     context= {'data': filename}
 
-        # return HttpResponse('file upload success')
-        return render(request, 'dogonicon/image_result.html', context)
+    #     # return HttpResponse('file upload success')
+    #     return render(request, 'portfolio/image_result.html', context)
+
+
+
+# def index(request):
+#     file = request.FILES.get('filename', '')
+#     filename = file._name
+
+#     if file == '':
+#         return HttpResponse('file을 다시 upload해주세요')
+#     else:
+#         fp = open(settings.BASE_DIR + '/static/portfolio/' + filename, 'wb')
+#         for chunk in file.chunks():
+#             fp.write(chunk)
+#         fp.close()
+
+#     return HttpResponse('file upload success')
+
+# class UploadView(View):
+#     def get(self, request):
+#         return render(request, 'portfolio/image_upload.html')
+
+#     def post(self, request):
+#         file = request.FILES.get('filename', '')
+#         filename = file._name
+
+#         if file == '':
+#             return HttpResponse('file을 다시 upload해주세요')
+#         else:
+#             fp = open(settings.BASE_DIR + '/static/portfolio/' + filename, 'wb')
+#             for chunk in file.chunks():
+#                 fp.write(chunk)
+#             fp.close()
+         
+#         context= {'data': filename}
+
+#         # return HttpResponse('file upload success')
+#         return render(request, 'portfolio/image_result.html', context)
